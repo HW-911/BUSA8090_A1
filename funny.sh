3dc84e88024fb18c15af68ac010e99665b11a4a3
HW-911 <63285979+HW-911@users.noreply.github.com>
2021-03-18 20:51:40 +0800

    Update funny.sh

--- funny.sh
+++ funny.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #save as funny.sh
-
+                                        # $# refers to the number of positional argument ($1, $2, $3, ...) (parameter 1, 2, 3, ...)
 if [ "$#" -gt 0 ];then                  # $# > 0
-        echo "This is funny"
+        echo "This is funny"                
 else                                    # $# <=0
         echo "This is NOT funny"
 fi
